da6d5196a8b0d2f4f5d6bcda508010f471e6b37f
kudr.max <mdkudryashov@edu.hse.ru>
2023-06-02 01:46:48 +0300

    Moved some code in cpp

--- source/game/game_builder.cpp
+++ source/game/game_builder.cpp
@@ -1,3 +1,56 @@
 #include "game_builder.h"
 
 
+void SimpleGameBuilder::create_rooms() {
+    int size_room_x = 100;
+    int size_room_y = 100;
+    size_t count_of_room_x = 2;
+    size_t count_of_room_y = 2;
+    std::vector<std::shared_ptr<Room>> vec;
+    for (size_t i_x = 0; i_x < count_of_room_x; ++i_x) {
+        for (size_t i_y = 0; i_y < count_of_room_y; ++i_y) {
+            vec.emplace_back(std::make_shared<Room>(m_room_size));
+        }
+        m_rooms.push_back(vec);
+    }
+}
+
+void SimpleGameBuilder::set_rooms_sides() {
+    for (auto& row: m_rooms) {
+        for (auto& room: row) {
+            for (size_t i = 0; i < 4; ++i) {
+                room->set_side(static_cast<Room::Direction>(i), std::make_shared<Wall>(*room));
+            }
+        }
+    }
+}
+
+void SimpleGameBuilder::create_state(IStateManager* state_manager, std::string window_title) {
+    m_game_state = std::make_unique<GameState>(state_manager, window_title);
+}
+
+void SimpleGameBuilder::set_all_to_state() {
+//        m_game_state->set_context(m_context);
+    std::vector<std::shared_ptr<Room>> new_vec;
+    for (auto&& v: m_rooms) {
+        new_vec.insert(new_vec.end(), v.begin(), v.end());
+    }
+    m_game_state->set_maze(Maze{ new_vec });
+}
+
+SimpleGameBuilder::SimpleGameBuilder(float width, float height, float room_size) :
+        m_width(width), m_height(height), m_room_size(room_size) {};
+
+GameBuilderDirector::GameBuilderDirector(std::unique_ptr<IGameBuilder> ptr_builder, std::string window_title,
+                                         float dynamic_objects_ratio) :
+        m_ptr_builder(std::move(ptr_builder)), m_window_title(window_title),
+        m_dynamic_objects_ratio(dynamic_objects_ratio) {}
+
+std::unique_ptr<GameState> GameBuilderDirector::build(IStateManager* state_manager) {
+    m_ptr_builder->create_rooms();
+    m_ptr_builder->set_rooms_sides();
+//        m_ptr_builder->create_context();
+    m_ptr_builder->create_state(state_manager, m_window_title);
+    m_ptr_builder->set_all_to_state();
+    return m_ptr_builder->get_game();
+};
--- source/game/game_builder.h
+++ source/game/game_builder.h
@@ -15,50 +15,13 @@ struct IGameBuilder {
 
 class SimpleGameBuilder : public IGameBuilder {
 public:
-
-    void create_rooms() override {
-        int size_room_x = 100;
-        int size_room_y = 100;
-        size_t count_of_room_x = 2;
-        size_t count_of_room_y = 2;
-        std::vector<std::shared_ptr<Room>> vec;
-        for (size_t i_x = 0; i_x < count_of_room_x; ++i_x) {
-            for (size_t i_y = 0; i_y < count_of_room_y; ++i_y) {
-                vec.emplace_back(std::make_shared<Room>(m_room_size));
-            }
-            m_rooms.push_back(vec);
-        }
-    };
-
-    void set_rooms_sides() override {
-        for (auto& row: m_rooms) {
-            for (auto& room: row) {
-                for (size_t i = 0; i < 4; ++i) {
-                    room->set_side(static_cast<Room::Direction>(i), std::make_shared<Wall>(*room));
-                }
-            }
-        }
-    };
-
+    SimpleGameBuilder(float width, float height, float room_size);
+    void create_rooms() override;
+    void set_rooms_sides() override;
     void create_context(float dynamic_objects_ratio) override {};
-
-    void create_state(IStateManager* state_manager, std::string window_title) override {
-        m_game_state = std::make_unique<GameState>(state_manager, window_title);
-    };
-
-    void set_all_to_state() override {
-//        m_game_state->set_context(m_context);
-        std::vector<std::shared_ptr<Room>> new_vec;
-        for (auto&& v: m_rooms) {
-            new_vec.insert(new_vec.end(), v.begin(), v.end());
-        }
-        m_game_state->set_maze(Maze{ new_vec });
-    };
-
+    void create_state(IStateManager* state_manager, std::string window_title) override;
+    void set_all_to_state() override;
     std::unique_ptr<GameState> get_game() override { return std::move(m_game_state); };
-
-    SimpleGameBuilder(float width, float height, float room_size) :
-            m_width(width), m_height(height), m_room_size(room_size) {};
 private:
     float m_width;
     float m_height;
@@ -89,21 +52,8 @@ private:
 class GameBuilderDirector {
 public:
     GameBuilderDirector(std::unique_ptr<IGameBuilder> ptr_builder, std::string window_title,
-                        float dynamic_objects_ratio) :
-            m_ptr_builder(std::move(ptr_builder)), m_window_title(window_title),
-            m_dynamic_objects_ratio(dynamic_objects_ratio) {};
-
-    std::unique_ptr<GameState> build(IStateManager* state_manager) {
-        state_manager->call();
-        m_ptr_builder->create_rooms();
-        std::cout << "GameBuilderDirector::build is started" << std::endl;
-        m_ptr_builder->set_rooms_sides();
-//        m_ptr_builder->create_context();
-        m_ptr_builder->create_state(state_manager, m_window_title);
-        m_ptr_builder->set_all_to_state();
-        std::cout << "GameBuilderDirector::build is ended" << std::endl;
-        return m_ptr_builder->get_game();
-    };
+                        float dynamic_objects_ratio);
+    std::unique_ptr<GameState> build(IStateManager* state_manager);
 private:
     float m_dynamic_objects_ratio;
     std::string m_window_title;
