22613ba56e465fb0f04d2515b6f50200332e94d7
kudr.max <mdkudryashov@edu.hse.ru>
2023-06-14 21:03:06 +0300

    Added implimintaeion of clone for GameContext

--- source/game/game_contex.h
+++ source/game/game_contex.h
@@ -8,7 +8,16 @@
 
 
 struct GameContext {
-    GameContext clone() { return *this; };
+    GameContext clone() {
+        GameContext new_context;
+        new_context.pacman = pacman;
+        new_context.state = state;
+        for(const auto& dynamic_object : dynamic_objects)
+            new_context.dynamic_objects.push_back(dynamic_object->clone());
+        for(const auto& static_object : static_objects)
+            new_context.static_objects.push_back(static_object->clone());
+        return new_context;
+    };
     enum State {
         INGAME, WIN, LOST
     };
