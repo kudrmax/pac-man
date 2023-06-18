ba32291657480e16044ce104c0dc009da0badc9f
kudr.max <mdkudryashov@edu.hse.ru>
2023-06-19 02:08:59 +0300

    Added exception in DeleteStaticEntity::handle

--- source/game/game_event.cpp
+++ source/game/game_event.cpp
@@ -3,6 +3,8 @@
 void DeleteStaticEntity::handle(GameContext& context) {
     auto& vec = context.static_objects;
     auto it = std::find_if(vec.begin(), vec.end(), [this](const auto& el) { return el.get() == &m_ptr_entity; });
+    if (it == vec.end())
+        throw std::runtime_error{ "There is no object for delete in DeleteStaticEntity" };
     vec.erase(it);
 }
 
