30088a203489e11b2c9784271b625d3186563e84
kudr.max <mdkudryashov@edu.hse.ru>
2023-06-19 03:06:02 +0300

    Added setFramerateLimit to IWindowKeeper::IWindowKeeper

--- source/i_window.cpp
+++ source/i_window.cpp
@@ -1,4 +1,8 @@
 #include "i_window.h"
+#include "config.h"
 #include <SFML/Graphics.hpp>
 
-IWindowKeeper::IWindowKeeper(const sf::VideoMode& mode, const std::string& title) : m_window(mode, title) {}
+IWindowKeeper::IWindowKeeper(const sf::VideoMode& mode, const std::string& title) : m_window(mode, title) {
+    m_window.setFramerateLimit(config::FRAME_RATE_LIMIT);
+//    m_window.setKeyRepeatEnabled(false);
+}
